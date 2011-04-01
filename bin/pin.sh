c66264d83087482a9195f9119740175334556ee6
Dustin Lacewell <dustin@contactmaverick.com>
2011-04-01 14:00:21 -0500

    Renamed some missed files.

--- bin/pin.sh
+++ bin/pin.sh
@@ -5,25 +5,6 @@ pin () {
     __sourceit
 }
 
-cd() {
-    builtin cd "$@"
-    __autoit
-}
-
-pushd() {
-    builtin pushd "$@"
-    __autoit
-}
-
-popd() {
-    builtin popd "$@"
-    __autoit
-}
-
-__autoit() {
-    __pinauto `pwd`
-    __sourceit
-}
 
 __sourceit () {
     SOURCEFILE=~/.pinconf/source.sh
