2d2fdc83ce6a84d1b9538b9476d7cf96106fd017
Dustin Lacewell <dlacewell@gmail.com>
2011-04-06 16:35:17 -0500

    Removed configuration on install stuff

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ from pin import VERSION
 
 setup(
     name='pin',
-    version="0.1",
+    version="0.1.1",
     packages=['pin', 'pin.plugins'],
     scripts=['bin/pin.sh', 'bin/__pin', ],
-    data_files=[(os.path.expanduser('~/.pinconf'), ['settings.yml'])],
     install_requires=['PyYAML', 'argparse'],
     provides=['pin'],
     author="Dustin Lacewell",
@@ -19,5 +18,5 @@ setup(
     long_description=open('README.markdown').read(),
 )
 
-user = os.path.basename(os.path.expanduser("~"))
-os.system('chown -R %s:%s %s' % (user, user, os.path.expanduser("~/.pinconf")))
+
+
